4d4acc35f7442c491ab6693cb1bcf8bf1b8351e6
Dom Rodriguez <shymega@shymega.org.uk>
2022-02-14 17:55:59 +0000

    Initial commit
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ src/drivers/macos/src/lib.rs
@@ -0,0 +1,2 @@
+//! macOS drivers has not been developed yet. The main developer does not own a Mac, and therefore
+//! cannot work on the driver. Contributions welcome.
---
+++ src/drivers/windows/src/lib.rs
@@ -0,0 +1,1 @@
+//! The Windows USB-over-IP USB driver has not been developed yet.
