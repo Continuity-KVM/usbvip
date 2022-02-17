fc416074027c80befee0c2e012245d2444a44cd2
Dom Rodriguez <shymega@shymega.org.uk>
2022-02-17 12:17:47 +0000

    Rename to USB-via-IP
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/drivers/macos/src/lib.rs
+++ src/drivers/macos/src/lib.rs
@@ -1,2 +1,16 @@
 //! macOS drivers has not been developed yet. The main developer does not own a Mac, and therefore
 //! cannot work on the driver. Contributions welcome.
+#![deny(
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs,
+    clippy::all,
+    clippy::cargo,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unused_import_braces,
+    unused_qualifications,
+    unused_extern_crates,
+    variant_size_differences
+)]
--- src/drivers/windows/src/lib.rs
+++ src/drivers/windows/src/lib.rs
@@ -1,1 +1,15 @@
-//! The Windows USB-over-IP USB driver has not been developed yet.
+//! The Windows USB-via-IP USB driver has not been developed yet.
+#![deny(
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs,
+    clippy::all,
+    clippy::cargo,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unused_import_braces,
+    unused_qualifications,
+    unused_extern_crates,
+    variant_size_differences
+)]
