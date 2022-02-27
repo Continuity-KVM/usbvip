7b4e4beb1be078684ecef440aacf69445d23c09b
Dom Rodriguez <shymega@shymega.org.uk>
2022-02-27 21:45:44 +0000

    Add common crate
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ src/common/src/lib.rs
@@ -0,0 +1,16 @@
+//! The common utilities crate for USB-via-IP.
+#![deny(
+    warnings,
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs,
+    clippy::all,
+    clippy::cargo,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unused_import_braces,
+    unused_qualifications,
+    unused_extern_crates,
+    variant_size_differences
+)]
